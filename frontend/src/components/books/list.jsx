a64e6c6f7926e77a4bda290c1e3dfd003b30b75d
Ryan <ryan.deisler@capgemini.com>
2021-10-20 21:39:42 -0400

    added loading message and search box (no functionality)

--- frontend/src/components/books/list.jsx
+++ frontend/src/components/books/list.jsx
@@ -1,28 +1,39 @@
 import { React, Component } from "react";
-import { Table, TableContainer, TableCell, TableHead, TableRow, TableBody, Typography, TextField }
+import { Table, TableContainer, TableCell, TableHead, TableRow, TableBody, Typography, }
     from "@material-ui/core";
 import {Paper} from "@material-ui/core";
 
 class BookList extends Component {
 
-    state = {
-        books: []
-    };
+    constructor(props) {
+            super(props);
+            this.state = {
+                books: [],
+                loading: true
+            }
+        }
+
 
     async componentDidMount() {
         const response = await fetch('/api/books');
         const body = await response.json();
         this.setState({books: body});
+        if (this.state.books != null)
+        {
+            this.setState({loading: false});
+        }
     }
 
     render() {
-        const {books} = this.state;
+        const books = this.state.books;
 
         return (
             <Paper elevation={3} className="book-list">
                 <Typography variant="h4" color="inherit" component="div">
                     <b>List of Books</b>
                 </Typography>
+                <br/>
+                <div class="ui icon input"><input type="text" placeholder="Search..."/><i aria-hidden="true" class="search icon"></i></div>
                 <TableContainer>
                     <Table sx={{minWidth: 0}}>
                         <TableHead>
@@ -45,7 +56,7 @@ class BookList extends Component {
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {books.map(book =>
+                            {this.state.loading ? <h3>Loading, please wait...</h3> : books.map(book =>
                                 <TableRow
                                     key={book.title}
                                     sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
